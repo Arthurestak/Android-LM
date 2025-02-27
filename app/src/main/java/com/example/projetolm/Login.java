de43ec9416294fd6c9a4c17fa8adb6b668fec252
Arthur Lopes Oliveira <Arthur51826596@edu.df.senac.br>
2025-02-27 17:34:46 -0300

    inicial

---
+++ app/src/main/java/com/example/projetolm/Login.java
@@ -0,0 +1,50 @@
+package com.example.projetolm;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Login extends AppCompatActivity {
+    EditText emailInput;
+    EditText senhaInput;
+    ImageView btEntrar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+emailInput = findViewById(R.id.emailInput);
+senhaInput = findViewById(R.id.senhaInput);
+btEntrar = findViewById(R.id.btEntrar);
+
+btEntrar.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+
+    }
+});
+
+
+
+
+
+
+
+    }
+}
